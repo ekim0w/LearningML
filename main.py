56983791b7c23084c22bed8b8c98fcea31baea1c
Kirill Ekimov <ekimovkirill94@gmail.com>
2025-09-16 23:00:36 +0300

    meow

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import RMSprop
 
-df = pd.read_csv("name.csv", sep =";", decimal = ',')  # замени на имя своего файла
+# Загружаем датасет
+df = pd.read_csv("filename.csv", sep=";", decimal= ',')
 
 print(df.head())
 print(df.columns)
@@ -14,7 +15,7 @@ print(df.columns)
 X = df[["Protein", "Fats", "Carbo"]].values
 y = df["Callories"].values
 
-# 2 Масштабирование и train/test
+# Масштабирование и train/test
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
@@ -22,10 +23,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-# 3 Модеlь
+# Модель
 model = Sequential([
-    Dense(16, activation='relu', input_shape=(X_train.shape[1],)),
-    # Dense(16, activation='relu'),
+    Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
     Dense(1, activation='linear')
 ])
 
@@ -33,10 +33,10 @@ model.compile(optimizer=RMSprop(learning_rate=0.001),
               loss='mse',
               metrics=['mae'])
 
-# 4 Обучение
+# Обучение
 model.fit(X_train, y_train, validation_split=0.2, epochs=50, batch_size=32, verbose=1)
 
-# 5 Проверка
+# Проверка
 loss, mae = model.evaluate(X_test, y_test)
 print(f"Средняя ошибка: {mae:.2f} ккал")
 
